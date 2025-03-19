d429ddcb960701c5fd38013225cb44e01bbd8d93
sebas <sebas@Sebas.home>
2025-03-19 18:19:18 +0100

    Refactorización 3- hemos extraido el bucle de getRent y hemos creado calcMonopoliesRent

--- monopoly/monopoly/edu/uhu/monopoly/PropertyCell.java
+++ monopoly/monopoly/edu/uhu/monopoly/PropertyCell.java
@@ -27,13 +27,19 @@ public class PropertyCell extends Cell {
 	public int getRent() {
 		int rentToCharge = rent;
 		String [] monopolies = proprietary.getMonopolies();
+		rentToCharge = calcMonopoliesRent(rentToCharge, monopolies);
+		if(numHouses > 0) {
+			rentToCharge = rent * (numHouses + 1);
+		}
+		return rentToCharge;
+	}
+
+	private int calcMonopoliesRent(int rentToCharge, String[] monopolies) {
 		for(int i = 0; i < monopolies.length; i++) {
 			if(monopolies[i].equals(colorGroup)) {
 				rentToCharge = rent * 2;
-			}
-		}
-		if(numHouses > 0) {
-			rentToCharge = rent * (numHouses + 1);
+			} 
+			
 		}
 		return rentToCharge;
 	}
